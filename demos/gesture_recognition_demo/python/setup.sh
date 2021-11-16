b2d4d0fb1fd653d8af7c0049862a1fac92a50820
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-16 09:19:23 +0800

    Update setup.sh

--- demos/gesture_recognition_demo/python/setup.sh
+++ demos/gesture_recognition_demo/python/setup.sh
@@ -1,4 +1,4 @@
-source /opt/ov2021.4-venv/bin/activate
+#source /opt/ov2021.4-venv/bin/activate
 python3 gesture_recognition_demo.py \
     -m_a asl-recognition-0004.xml \
     -m_d person-detection-asl-0001.xml \
