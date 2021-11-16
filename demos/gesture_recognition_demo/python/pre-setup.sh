6abb9525770d7a931fe962ec18634c5b5bb2a6f8
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-16 03:32:08 -0500

    Add files via upload

---
+++ demos/gesture_recognition_demo/python/pre-setup.sh
@@ -0,0 +1,2 @@
+docker pull openvino/ubuntu18_data_dev
+docker run -it --device /dev/dri:/dev/dri --device-cgroup-rule='c 189:* rmw' -v /dev/bus/usb:/dev/bus/usb --rm openvino/ubuntu18_data_dev:latest 
