98db05c6b724af7609c9923bdf4cd82018d14d72
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-17 10:11:32 +0800

    Update pre-setup.sh

--- demos/gesture_recognition_demo/python/pre-setup.sh
+++ demos/gesture_recognition_demo/python/pre-setup.sh
@@ -1,2 +1,4 @@
 pip install --upgrade pip
 pip install -r requirements.txt 
+docker pull openvino/ubuntu18_data_dev
+docker run -it --device /dev/dri:/dev/dri --device-cgroup-rule='c 189:* rmw' -v /dev/bus/usb:/dev/bus/usb --rm openvino/ubuntu18_data_dev:latest
