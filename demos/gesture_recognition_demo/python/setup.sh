ce704d871ea6ebafd28c9d3c3958b8ab56a2e6ee
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-18 13:15:09 +0800

    Update setup.sh

--- demos/gesture_recognition_demo/python/setup.sh
+++ demos/gesture_recognition_demo/python/setup.sh
@@ -1,5 +1,6 @@
-docker pull openvino/ubuntu18_data_dev
-docker run -it --device /dev/dri:/dev/dri --device-cgroup-rule='c 189:* rmw' -v /dev/bus/usb:/dev/bus/usb --rm openvino/ubuntu18_data_dev:latest
+#docker pull openvino/ubuntu18_data_dev
+#docker run -it --device /dev/dri:/dev/dri --device-cgroup-rule='c 189:* rmw' -v /dev/bus/usb:/dev/bus/usb --rm openvino/ubuntu18_data_dev:latest
+/opt/intel/openvino_2021.4.752/opencv/setupvars.sh
 python3 gesture_recognition_demo.py \
     -m_a asl-recognition-0004.xml \
     -m_d person-detection-asl-0001.xml \
