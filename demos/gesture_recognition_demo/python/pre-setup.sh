62c0754cf3151146f246f2a62e835339126a3606
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-18 11:28:05 +0800

    Update pre-setup.sh

--- demos/gesture_recognition_demo/python/pre-setup.sh
+++ demos/gesture_recognition_demo/python/pre-setup.sh
@@ -1,183 +1,76 @@
-#!/usr/bin/env bash
-# author: joe.zheng
-# version: 21.9.15
+#!/bin/bash
 
-set -e
-
-SELF="$(basename $0)"
-
-PROXY_HOST="http://proxy-prc.intel.com"
-PROXY_PORT=""
-PROXY_SKIP="localhost,sh.intel.com,istio-system.svc,127.0.0.0/8,172.16.0.0/20,192.168.0.0/16,10.0.0.0/8"
-PORT_HTTP="911"
-PORT_HTTPS="912"
-REPO_MIRROR="https://docker.mirrors.ustc.edu.cn"
-
-DRY_RUN="n"
-
-HELP=$(cat <<EOF
-Usage: $SELF [-M <mirror>] [-N <noproxy>] [-H <port>] [-S <port>] [-n] [-h]
-  [<host>] [<port>]
-  Install Docker and configure it, e.g. proxy, mirror
-
-  -M <mirror>:  registry mirror, default: $REPO_MIRROR
-  -N <noproxy>: no_proxy settings, default: $PROXY_SKIP
-  -H <port>:    proxy port for http, default: $PORT_HTTP
-  -S <port>:    proxy port for https, default: $PORT_HTTPS
-  -n:           dry run, print out information only, default: $DRY_RUN
-  -h:           print the usage message
-
-  <host>:       proxy host, "" to disable proxy, default: $PROXY_HOST
-  <port>:       proxy port, override -HS options, default: $PROXY_PORT
-
-Examples:
-
-  1. Deploy with default configuration
-     $SELF
-
-  2. Deploy without proxy
-     $SELF ""
-
-  3. Deploy with specified proxy host and port (913 for http and https)
-     $SELF http://proxy.example.com 913
-
-  4. Deploy with specified proxy host and port (911 for http, 912 for https)
-     $SELF -H 911 -S 912 http://proxy.example.com
-EOF
-)
-
-while getopts ":N:M:H:S:nh" opt
-do
-  case $opt in
-    M ) REPO_MIRROR=$OPTARG;;
-    N ) PROXY_SKIP=$OPTARG;;
-    H ) PORT_HTTP=$OPTARG;;
-    S ) PORT_HTTPS=$OPTARG;;
-    n ) DRY_RUN='y';;
-    h ) echo "$HELP" && exit;;
-    * ) echo "no such option: $opt" && exit 1;;
-  esac
-done
-shift $((OPTIND-1))
-
-if (( $# >= 1 )); then
-  PROXY_HOST="$1"
-fi
-if (( $# >= 2 )); then
-  PROXY_PORT="$2"
-fi
-
-SERVICE_CONFIG="/etc/systemd/system/docker.service.d/http-proxy.conf"
-DOCKERD_CONFIG="/etc/docker/daemon.json"
-DOCKERC_CONFIG="~/.docker/config.json"
-
-# export proxy settings
-if [[ -n $PROXY_HOST ]]; then
-  export http_proxy=$PROXY_HOST:${PROXY_PORT:-$PORT_HTTP}
-  export https_proxy=$PROXY_HOST:${PROXY_PORT:-$PORT_HTTPS}
-  export no_proxy=$PROXY_SKIP
-else
-  export http_proxy=
-  export https_proxy=
-  export no_proxy=
-fi
-
-for v in DRY_RUN PROXY_HOST PROXY_PORT PROXY_SKIP PORT_HTTP PORT_HTTPS \
-  REPO_MIRROR SERVICE_CONFIG DOCKERD_CONFIG DOCKERC_CONFIG; do
-  eval echo "$v: \${$v}"
-done
-
-[[ $DRY_RUN == "y" ]] && exit
-
-echo "check prerequisites"
-if [[ -z $(which curl) ]]; then
-  echo "curl is not available"
-  exit 1
-fi
-if [[ -z $(which python3) ]]; then
-  echo "python3 is not available"
-  exit 1
-fi
-
-# ensure Docker is installed
-if [[ -z $(which docker) ]]; then
-  echo "docker is not available, install it"
-  curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
-  echo "docker installation is done"
-fi
-
-if [[ -e $SERVICE_CONFIG || -e $DOCKERD_CONFIG || -e $DOCKERC_CONFIG ]]; then
-  cat <<EOF
-any of the following configuration files already exist
-* $SERVICE_CONFIG
-* $DOCKERD_CONFIG
-* $DOCKERC_CONFIG
-EOF
-  read -p "overwrite them and continue? y/N: " result
-  if [[ $result != "y" ]]; then
-    exit
-  fi
-fi
-
-echo "configure docker client: $DOCKERC_CONFIG"
-DOCKERC_CONFIG="$DOCKERC_CONFIG" python3 <<'EOF'
-import os, json, pathlib, collections
-
-nest_dict = lambda: collections.defaultdict(nest_dict)
-
-f = pathlib.Path(os.environ['DOCKERC_CONFIG']).expanduser()
-d = nest_dict()
-
-try:
-  with f.open() as fh:
-    d.update(json.load(fh))
-except FileNotFoundError:
-  pass
-
-m = dict(httpProxy='http_proxy', httpsProxy='https_proxy', noProxy='no_proxy')
-d['proxies']['default'].update({ k : os.environ[v] for k, v in m.items() })
-
-f.parent.mkdir(parents=True, exist_ok=True)
-with f.open('w') as fh:
-  json.dump(d, fh, indent=4)
-EOF
-
-echo "configure docker daemon"
-echo "configure $SERVICE_CONFIG"
-sudo mkdir -p $(dirname $SERVICE_CONFIG)
-cat <<EOF | sudo tee $SERVICE_CONFIG
-[Service]
-Environment="HTTP_PROXY=$http_proxy"
-Environment="HTTPS_PROXY=$https_proxy"
-Environment="NO_PROXY=$no_proxy"
-EOF
-
-echo "configure $DOCKERD_CONFIG"
-sudo mkdir -p $(dirname $DOCKERD_CONFIG)
-mirrors=""
-if [[ -n $REPO_MIRROR ]]; then
-  mirrors="\"registry-mirrors\": [\"$REPO_MIRROR\"],"
-fi
-cat <<EOF | sudo tee $DOCKERD_CONFIG
-{
-  "log-opts": {
-    "max-size": "500m"
-  },
-  $mirrors
-  "insecure-registries": ["10.0.0.0/8", "127.0.0.0/8"]
+function msg() {
+  echo "> $@"
 }
-EOF
 
-echo "enable docker service"
-sudo systemctl enable docker
 
-echo "restart docker service"
-sudo systemctl daemon-reload
-sudo systemctl restart docker
 
-if groups | grep -qwv docker; then
-  echo "add docker group to $USER"
+function Install_docker() {
+  msg "Uninstall docker old versions"
+  sudo apt-get remove docker docker-engine docker.io containerd runc
+  
+  msg "Update the apt package index"
+  sudo apt-get update
+
+  msg "Install packages to allow apt to use a repository over HTTPS"
+  sudo apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release \
+
+  msg "Add Docker’s official GPG key"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+  msg "set up the stable repository"
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  msg "Update the apt package index, and install the latest version of Docker Engine and containerd"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+  msg "Docker installation complete"
+}
+
+
+
+function Configure_Docker_to_start_on_boot() {
+  msg "Configure Docker to start on boot"
+  sudo systemctl enable docker.service
+}
+
+
+
+function Manage_Docker_as_non-root_user() {
+  msg "Create the docker group"
+  sudo groupadd docker
+  
+  msg "Add the login user to the docker group"
   sudo usermod -aG docker $USER
-  echo "logout and login again or reboot to take effect"
-fi
-echo "done"
+  
+  msg "Logout and login again or reboot to take effect"
+}
+
+
+
+function Uninstall_Docker() {
+  msg "Uninstall the Docker Engine, CLI, and Containerd packages"
+  sudo apt-get purge docker-ce docker-ce-cli containerd.io
+  
+  msg "Delete all images, containers, and volumes"
+  sudo rm -rf /var/lib/docker
+  sudo rm -rf /var/lib/containerd
+}
+
+
+
+Install_docker
+Configure_Docker_to_start_on_boot
+Manage_Docker_as_non-root_user
+#Uninstall_Docker
+
+msg "Done"
+
+
