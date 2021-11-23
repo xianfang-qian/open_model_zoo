4741baae93daa6093067ea4a0fbcbc058b9c894e
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-23 10:10:57 +0800

    Update setup.sh

--- demos/gesture_recognition_demo/python/setup.sh
+++ demos/gesture_recognition_demo/python/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #mkdir -p ~/results
-source /opt/intel/openvino_2021/bin/setupvars.sh
+source /opt/intel/openvino_2021/bin/setupvars.sh && \
 python3 gesture_recognition_demo.py \
     -m_a asl-recognition-0004.xml \
     -m_d person-detection-asl-0001.xml \
