ea8e58640218ae7a0173adec0f0cedb58e429db6
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-23 10:53:26 +0800

    Create run.sh

---
+++ demos/gesture_recognition_demo/python/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /opt/intel/openvino_2021/bin/setupvars.sh &&\
+python3 gesture_recognition_demo.py \
+    -m_a asl-recognition-0004.xml \
+    -m_d person-detection-asl-0001.xml \
+    -i 0 
+    -c msasl100.json
