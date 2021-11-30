ac6d80a69fdd0e6de59af0d8239a0ba0aacbbaef
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-30 13:21:20 +0800

    Create run.sh

---
+++ demos/instance_segmentation_demo/python/run.sh
@@ -0,0 +1,8 @@
+source /opt/intel/openvino_2021/bin/setupvars.sh && \
+   python3 instance_segmentation_demo/instance_segmentation_demo.py \
+      -m instance-segmentation-security-0228.xml \
+     --label coco_80cl.txt \
+     --no_keep_aspect_ratio \
+     -i classroom.mp4 \
+     --delay 1
+     -o /data/outpit_instance_segmentation.mp4
