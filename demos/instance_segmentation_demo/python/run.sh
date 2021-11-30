13f50b5422511a445505cd208d9b3272b52c99f7
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-30 13:39:22 +0800

    Update run.sh

--- demos/instance_segmentation_demo/python/run.sh
+++ demos/instance_segmentation_demo/python/run.sh
@@ -1,6 +1,6 @@
 source /opt/intel/openvino_2021/bin/setupvars.sh && \
    python3 instance_segmentation_demo/instance_segmentation_demo.py \
-      -m instance-segmentation-security-0228.xml \
+      -m /opt/app-root/intel/instance-segmentation-security-0228/FP32/instance-segmentation-security-0228.xml \
      --label coco_80cl.txt \
      --no_keep_aspect_ratio \
      -i classroom.mp4 \
