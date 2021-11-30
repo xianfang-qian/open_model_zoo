4a3400550570368e85c1664f58f256563fb19603
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-30 13:28:47 +0800

    Update pre-run.sh

--- demos/instance_segmentation_demo/python/pre-run.sh
+++ demos/instance_segmentation_demo/python/pre-run.sh
@@ -1,5 +1,5 @@
 source /opt/intel/openvino_2021/bin/setupvars.sh
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 set INTEL_OPENVINO_DIR = /opt/intel/openvino_2021
 python3 ${INTEL_OPENVINO_DIR}/deployment_tools/open_model_zoo/tools/downloader/downloader.py --list models.lst && \
