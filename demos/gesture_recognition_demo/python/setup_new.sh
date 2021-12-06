cbbe06dcb627d95dff5117e90a29cd8f6a05c692
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-12-06 16:32:42 +0800

    Create setup_new.sh

---
+++ demos/gesture_recognition_demo/python/setup_new.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /opt/intel/openvino_2021/bin/setupvars.sh &&\
+python3 gesture_recognition_demo.py  -m_a asl-recognition-0004.xml -m_d person-detection-asl-0001.xml  -i gesture.jpg  -c msasl100.json -d CPU  --no_show -o output.jpg
+dir=`pwd`
+ls ${pwd}
